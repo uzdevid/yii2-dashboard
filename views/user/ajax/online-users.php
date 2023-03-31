d3def79a008815d2eb0f1cbfa796019ec91df699
uzdevid <uzdevid@gmail.com>
2023-03-31 18:05:42 +0500

    исправил вид списка пользователей в онлайн

--- views/user/ajax/online-users.php
+++ views/user/ajax/online-users.php
@@ -10,32 +10,32 @@ use uzdevid\dashboard\models\User;
  */
 ?>
 
-<?php foreach ($online_users as $user): ?>
+<?php foreach ($online_users as $_user): ?>
     <li class="online-user-item position-relative">
-        <img width="50px" src="<?php echo $user->profileImage; ?>" class="rounded-circle">
+        <img width="50px" height="50px" src="<?php echo $_user->profileImage; ?>" class="rounded-circle">
         <div class="ps-2">
-            <h4><?php echo $user->fullname; ?></h4>
+            <h4><?php echo $_user->fullname; ?></h4>
             <p>
                 <i class="bi bi-circle-fill text-success m-0" style="font-size: 12px;"></i>
-                <span><?php echo date('H:i', $user->last_activity_time); ?></span>
+                <span><?php echo date('H:i', $_user->last_activity_time); ?></span>
             </p>
         </div>
-        <?php echo ModalPage::link('', Url::to(['/system/user/view', 'id' => $user->id]), ['class' => 'stretched-link']); ?>
+        <?php echo ModalPage::link('', Url::to(['/system/user/view', 'id' => $_user->id]), ['class' => 'stretched-link']); ?>
     </li>
 <?php endforeach; ?>
-<? if (!empty($users)): ?>
+<?php if (!empty($users) && !empty($online_users)): ?>
     <hr>
 <?php endif; ?>
-<?php foreach ($users as $user): ?>
+<?php foreach ($users as $_user): ?>
     <li class="online-user-item position-relative">
-        <img width="50px" src="<?php echo $user->profileImage; ?>" class="rounded-circle">
+        <img width="50px" height="50px" src="<?php echo $_user->profileImage; ?>" class="rounded-circle">
         <div class="ps-2">
-            <h4><?php echo $user->fullname; ?></h4>
+            <h4><?php echo $_user->fullname; ?></h4>
             <p>
                 <i class="bi bi-circle-fill text-secondary m-0" style="font-size: 12px;"></i>
-                <span><?php echo date('H:i', $user->last_activity_time); ?></span>
+                <span><?php echo date('H:i', $_user->last_activity_time); ?></span>
             </p>
         </div>
-        <?php echo ModalPage::link('', Url::to(['/system/user/view', 'id' => $user->id]), ['class' => 'stretched-link']); ?>
+        <?php echo ModalPage::link('', Url::to(['/system/user/view', 'id' => $_user->id]), ['class' => 'stretched-link']); ?>
     </li>
 <?php endforeach; ?>
