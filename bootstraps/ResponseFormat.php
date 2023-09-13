831be81717251636d64c700423d53589fcc0ad45
uzdevid <uzdevid@gmail.com>
2023-09-13 18:10:03 +0500

    Добавил обработчик события BeforeAction

--- bootstraps/ResponseFormat.php
+++ bootstraps/ResponseFormat.php
@@ -2,17 +2,27 @@
 
 namespace uzdevid\dashboard\bootstraps;
 
+use Yii;
+use yii\base\Application;
 use yii\base\BootstrapInterface;
 use yii\web\Response;
 
 class ResponseFormat implements BootstrapInterface {
     public function bootstrap($app): void {
+        $app->on(Application::EVENT_AFTER_ACTION, function ($event) {
+            if (Yii::$app->request->isAjax) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+            }
+        });
+
         $app->response->on(Response::EVENT_BEFORE_SEND, function ($event) {
-            if ($event->sender->format !== Response::FORMAT_JSON) {
+            $response = $event->sender;
+
+            if ($response->format !== Response::FORMAT_JSON) {
                 return;
             }
 
-            $response = $event->sender;
+            $response->format = Response::FORMAT_JSON;
 
             if (!isset($response->data['success'])) {
                 $response->data = [
