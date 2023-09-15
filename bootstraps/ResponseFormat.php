b546fdf0f4f67cec500591eff17d2baefcc25f0b
uzdevid <uzdevid@gmail.com>
2023-09-15 16:03:16 +0500

    Удалил ненужный код

--- bootstraps/ResponseFormat.php
+++ bootstraps/ResponseFormat.php
@@ -2,34 +2,8 @@
 
 namespace uzdevid\dashboard\bootstraps;
 
-use Yii;
-use yii\base\Application;
 use yii\base\BootstrapInterface;
-use yii\web\Response;
 
 class ResponseFormat implements BootstrapInterface {
-    public function bootstrap($app): void {
-        $app->on(Application::EVENT_AFTER_ACTION, function ($event) {
-            if (Yii::$app->request->isAjax) {
-                Yii::$app->response->format = Response::FORMAT_JSON;
-            }
-        });
-
-        $app->response->on(Response::EVENT_BEFORE_SEND, function ($event) {
-            $response = $event->sender;
-
-            if ($response->format !== Response::FORMAT_JSON) {
-                return;
-            }
-
-            $response->format = Response::FORMAT_JSON;
-
-            if (!isset($response->data['success'])) {
-                $response->data = [
-                    'success' => $response->isSuccessful,
-                    'body' => $response->data,
-                ];
-            }
-        });
-    }
+    public function bootstrap($app): void { }
 }
